a627af8719ba770a647da97908df98ce972298db
Tilak Sharma <tilak.sharma.mail@gmail.com>
2020-08-16 19:41:12 +0530

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,9 @@
 import Head from 'next/head';
-import { useDispatch, useSelector } from 'react-redux';
 
 import HomePage from '../components/homepage';
 import PageHeader from '../components/page-header';
 
 const Index = () => {
-  //const dispatch = useDispatch();
-  //const {posts} = useSelector(state => state.posts);
-
   return (
     <div id='App'>
       <Head>
